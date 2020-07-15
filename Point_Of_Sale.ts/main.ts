d7563dc35e8027adb4bfc31d517afcf90b92e60f
Yossi <yosileyid@gmail.com>
2020-07-14 21:18:26 -0400

    Added filter function to Add_User

--- Point_Of_Sale.ts/main.ts
+++ Point_Of_Sale.ts/main.ts
@@ -1,5 +1,8 @@
 import { Hourly_Employee , Salaried_Employee } from "./user/Employee.ts"
-import { Add_User_To_db } from "./user/User_Actions.ts"
+import { Add_User } from "./user/User_Actions.ts"
+import { readJsonSync } from "https://deno.land/std/fs/mod.ts";
+
+
 
 // ============================
 // Create a new Hourly Employee
@@ -9,6 +12,20 @@ const john_smith = new Hourly_Employee(
     
     // Full Name
     "John", 
+    "Smith",
+    
+    // Date of birth
+    { month: 1 , day: 12 , year: 1998 } , 
+    
+    // Hourly Wage
+    { wage: 22 , rate: "weekly" } 
+
+)
+
+const jay_smith = new Hourly_Employee(
+    
+    // Full Name
+    "Jay", 
     "Smith",
     
     // Date of birth
@@ -33,5 +50,24 @@ const gary_smith = new Salaried_Employee(
 
 )
 
-Add_User_To_db(john_smith)
-Add_User_To_db(gary_smith)
+const Santa = new Salaried_Employee(
+    
+    // Full Name
+    "Santa", 
+    "Smith",
+    
+    // Date of birth
+    { month: 4 , day: 1 , year: 1998 } , 
+    
+    // Salary Wage
+    { wage: 2200 , rate: "monthly" } 
+
+)
+
+Add_User(john_smith)
+
+Add_User(gary_smith)
+
+Add_User(jay_smith)
+
+Add_User(Santa)
--- Point_Of_Sale.ts/user/User_Actions.ts
+++ Point_Of_Sale.ts/user/User_Actions.ts
@@ -1,16 +1,38 @@
 import { readJsonSync, writeJsonSync } from "https://deno.land/std/fs/mod.ts";
 
-export const Add_User_To_db = (user: any) => {
+export const Add_User = (user: any) => {
     let data: any = readJsonSync("./user/employee_list.json")
-
-    if(user.pay.rate === "weekly") {
-        data.employees.hourly[user.fn+"_"+user.ln] = user
-        writeJsonSync( "./user/employee_list.json", data )
-    } else if(user.pay.rate === "monthly") {
-        data.employees.salaried[user.fn+"_"+user.ln] = user
-        writeJsonSync( "./user/employee_list.json", data )
-    }
+    let hourly_employee: any = data.employees.hourly
+    let salaried_employee: any = data.employees.salaried
+    let temp_array: string[] = []
     
-    console.log(`${JSON.stringify(user.fn +" "+ user.ln + " added to db")}`)
+    for (var name in hourly_employee) {
+        temp_array.push(name)
+    }
+
+    for (var name in salaried_employee) {
+        temp_array.push(name)
+    }
+
+
+    let username = temp_array.filter(elem =>  elem === user.fn+"_"+user.ln)
+    if (username.length > 0) {
+ 
+        console.log(`User "${username[0]}" already in db`)
+ 
+    } else if(user.pay.rate === "weekly") {
+        
+        data.employees.hourly[user.fn+"_"+user.ln] = user
+        writeJsonSync( "./user/employee_list.json", data , {spaces: 2})
+        console.log(`Added "${user.fn +"_"+ user.ln}"`)
+
+        
+    } else if(user.pay.rate === "monthly") {
+        
+        data.employees.salaried[user.fn+"_"+user.ln] = user
+        writeJsonSync( "./user/employee_list.json", data , {spaces: 2})
+        console.log(`Added "${user.fn +"_"+ user.ln}"`)
+        
+    }
 
 }
